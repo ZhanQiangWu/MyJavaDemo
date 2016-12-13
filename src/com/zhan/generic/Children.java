797541e65f270691174bf5a6b6170d09a6234bf1
wuzhanqiang <wuzhanqiang@wxchina.com>
2016-12-13 13:55:32 +0800

    泛型module添加

---
+++ src/com/zhan/generic/Children.java
@@ -0,0 +1,11 @@
+package com.zhan.generic;
+
+/**
+ *  Created by zhan on 2016/12/13.
+ */
+public class Children {
+
+    public static void main(String args[]){
+        System.out.print("hello wordtest");
+    }
+}

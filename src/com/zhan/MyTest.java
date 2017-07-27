dd16fd40e64501e8d42ea527f35b9c864145b1dc
wuzhanqiang <wuzhanqiang@wxchina.com>
2017-07-27 09:00:18 +0800

    有更新

--- src/com/zhan/MyTest.java
+++ src/com/zhan/MyTest.java
@@ -1,21 +1,21 @@
 package com.zhan;
 
+import java.util.ArrayList;
+
 /**
  *  Created by zhan on 2016/12/27.
  */
 public class MyTest {
 
-    public static void main(String args[]){
-        Dog dog1 = new Dog("haah");
-        Dog dog2 = new Dog("haah");
-        System.out.println(dog1 == dog2);
-        System.out.println(dog1.equals(dog2));
-    }
-
-    static class Dog{
-        String name;
-        Dog(String name){
-            this.name = name;
+    static void printArray(Object...args){
+        for (Object obj:args){
+            System.out.println(obj+" ");
         }
     }
+
+    public static void main(String args[]){
+        printArray();
+
+    }
 }
+

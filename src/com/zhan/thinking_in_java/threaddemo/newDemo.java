36b99f37657050feeb67d814bd0252106a4389b6
ZhanQiangWu <wuzhanqiang2012@m.scnu.edu.cn>
2017-09-27 07:05:37 +0800

    泛型数组

--- src/com/zhan/thinking_in_java/threaddemo/newDemo.java
+++ src/com/zhan/thinking_in_java/threaddemo/newDemo.java
@@ -1,8 +1,22 @@
 package com.zhan.thinking_in_java.threaddemo;
 
+import static com.zhan.thinking_in_java.threaddemo.newDemo.ArrayOfGenericReference.gia;
+
 /**
  * http://825635381.iteye.com/blog/2184680
  * Created by zhan on 2017/9/11.
  */
 public class newDemo {
+
+    public static void main(String args[]){
+        gia = (Generic<Integer>[]) new Generic[2];
+    }
+
+    static class Generic<T>{
+
+    }
+
+    public static class ArrayOfGenericReference{
+        static Generic<Integer>[] gia;
+    }
 }

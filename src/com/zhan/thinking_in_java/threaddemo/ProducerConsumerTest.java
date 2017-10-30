dea5d2333be950d2b0c0607f15693438a3c58f1b
wuzhanqiang <wuzhanqiang@wxchina.com>
2017-10-30 18:07:48 +0800

    线程测试

--- src/com/zhan/thinking_in_java/threaddemo/ProducerConsumerTest.java
+++ src/com/zhan/thinking_in_java/threaddemo/ProducerConsumerTest.java
@@ -15,12 +15,14 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ProducerConsumerTest {
 
     public static void main(String[] args) {
-
+        test(new AwaitSignalStorage());
     }
 
-    void waitNotifyTest(){
+
+
+    static void test(Storage storage){
         //仓库对象
-        Storage storage = new Storage();
+        //Storage storage = new AwaitSignalStorage();
 
         //生产者对象
         Producer p1 = new Producer(storage);
@@ -51,9 +53,7 @@ public class ProducerConsumerTest {
         c3.setNum(30);
 
         // 线程开始执行
-        c1.start();
-        c2.start();
-        c3.start();
+
         p1.start();
         p2.start();
         p3.start();
@@ -61,6 +61,10 @@ public class ProducerConsumerTest {
         p5.start();
         p6.start();
         p7.start();
+
+        c1.start();
+        c2.start();
+        c3.start();
     }
 }
 
@@ -93,8 +97,18 @@ class AwaitSignalStorage extends Storage{
 
     // 生产num个产品
     public void produce(int num){
+        System.out.println("【要生产的产品数量】:" + num + "-----> " + "进来了");
         //获得锁
         lock.lock();
+
+        for (int i = 0; i < 3 ; i++){
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("【要生产的产品数量】:" + num + "/t 睡眠:" + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
         // 如果仓库剩余容量不足
         while (list.size() + num > MAX_SIZE){
@@ -114,13 +128,73 @@ class AwaitSignalStorage extends Storage{
         }
         System.out.println("【已经生产产品数】:" + num + "/t【现仓储量为】:" + list.size());
         // 唤醒其他所有线程
+        System.out.println("【要生产的产品数量】:" + num + "-----> " + "解锁了");
         full.signalAll();
         empty.signalAll();
 
         //释放锁
         lock.unlock();
     }
+
+    // 消费num个产品
+    public void consume(int num){
+        System.out.println("【要消费的产品数量】:" + num + "-----> " + "进来了");
+        //获得锁
+        lock.lock();
+
+        for (int i = 0; i < 3 ; i++){
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("【要消费的产品数量】:" + num + "/t 睡眠:" + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // 如果仓库存储量不足
+        while (list.size() < num){
+            System.out.println("【要消费的产品数量】:" + num + "/t【库存量】:" + list.size()
+                    + "/t暂时不能执行生产任务!");
+            try {
+                // 由于条件不满足，消费阻塞
+                empty.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 消费条件满足情况下，消费num个产品
+        for (int i = 1; i <= num; ++i) {
+            list.remove();
+        }
+
+        System.out.println("【已经消费产品数】:" + num + "/t【现仓储量为】:" + list.size());
+
+        // 唤醒其他所有线程
+        System.out.println("【要消费的产品数量】:" + num + "-----> " + "解锁了");
+        full.signalAll();
+        empty.signalAll();
+
+        // 释放锁
+        lock.unlock();
+    }
+
+    // set/get方法
+    public int getMAX_SIZE()
+    {
+        return MAX_SIZE;
+    }
+
+    public LinkedList<Object> getList()
+    {
+        return list;
+    }
+
+    public void setList(LinkedList<Object> list)
+    {
+        this.list = list;
+    }
 }
+
 
 
 /**
